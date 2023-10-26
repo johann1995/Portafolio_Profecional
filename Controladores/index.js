bcae6a582b2db83071246f5f509bc27d25dbf38d
johann chavarro <chavarrogarciajohann@gmail.com>
2023-10-26 11:50:19 -0500

    Modi_26/10/23_vs_15
    es donde agrego código js para intentar dar funcionalidad al formulario y mejoro el front-end

--- Controladores/index.js
+++ Controladores/index.js
@@ -1,3 +1,4 @@
+/*boton de watsapp */
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -9,4 +10,41 @@ navToggle.addEventListener("click", () => {
   } else {
     navToggle.setAttribute("aria-label", "Abrir menú");
   }
+});
+
+
+/*para el formulario de contacto*/
+
+
+// Obtén una referencia a los elementos del formulario
+const form = document.querySelector('.contact__form-wrapper');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const subjectInput = document.getElementById('subject');
+const messageInput = document.getElementById('message');
+
+// Agrega un evento de escucha para el envío del formulario
+form.addEventListener('submit', function (event) {
+  event.preventDefault(); // Evita que se envíe el formulario de manera predeterminada
+
+  // Obtén los valores de los campos de entrada
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const subject = subjectInput.value;
+  const message = messageInput.value;
+
+  // Haz lo que necesites con los datos (por ejemplo, mostrarlos en la consola)
+  console.log('Nombre:', name);
+  console.log('Correo Electrónico:', email);
+  console.log('Asunto:', subject);
+  console.log('Mensaje:', message);
+ 
+
+  // Puedes enviar los datos a un servidor o realizar otras acciones aquí
+
+  // Limpia los campos de entrada
+  nameInput.value = '';
+  emailInput.value = '';
+  subjectInput.value = '';
+  messageInput.value = '';
 });
